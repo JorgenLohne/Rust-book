87737eac25032089e1c249a3d1a4e6389d8f3e94
Jørgen Lohne <jorgen.lohne@tietoevry.com>
2022-10-19 15:34:12 +0200

    initial commit

---
+++ guessing_game/target/debug/guessing_game.d
@@ -0,0 +1,1 @@
+/Users/jorgenlohne/Documents/git/rust-book/guessing_game/target/debug/guessing_game: /Users/jorgenlohne/Documents/git/rust-book/guessing_game/src/main.rs
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/jorgenlohne/Documents/git/rust-book/hello_cargo/target/debug/hello_cargo: /Users/jorgenlohne/Documents/git/rust-book/hello_cargo/src/main.rs
